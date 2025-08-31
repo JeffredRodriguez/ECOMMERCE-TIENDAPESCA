f5c79813ffe5a03a0671f61bf150f5e55f404046
JeffredRodriguez <Jeffredra1306@gmail.com>
2025-08-30 18:59:36 -0600

    version 1.0.0

--- src/main/java/com/tiendapesca/APItiendapesca/ApItiendapescaApplication.java
+++ src/main/java/com/tiendapesca/APItiendapesca/ApItiendapescaApplication.java
@@ -1,22 +1,15 @@
 package com.tiendapesca.APItiendapesca;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.tiendapesca.APItiendapesca.Security.appconfig;
 
 
 @SpringBootApplication
 public class ApItiendapescaApplication {
 
-	public static void main(String[] args) {
-	
-	{
-
-    SpringApplication.run(ApItiendapescaApplication.class, args);
+    public static void main(String[] args) {
+      
+        SpringApplication.run(ApItiendapescaApplication.class, args);
+    }
 }
-
-	
-	}}
-
-
